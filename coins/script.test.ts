9e087ed97007b398a4d143ccfd9e3244c05acd64
kento <kento@toriwatari.bird>
2022-04-05 20:09:38 +0900

    ✨ Update

---
+++ coins/script.test.ts
@@ -0,0 +1,11 @@
+import { coins } from "./script"
+
+describe.each([
+  [1, 0, 0, 500, 1],
+  [1, 5, 0, 500, 2],
+  [1, 5, 10, 500, 7]
+])("coins", (a, b, c, x, expected) => {
+  test("main", () => {
+    expect(coins(a, b, c, x)).toBe(expected)
+  })
+})
