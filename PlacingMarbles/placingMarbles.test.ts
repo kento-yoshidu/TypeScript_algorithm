69bbe014100c16d408dfd57545a0a6450fe72ce7
kento <kento@toriwatari.bird>
2022-04-07 19:31:59 +0900

    ✨ Update

---
+++ PlacingMarbles/placingMarbles.test.ts
@@ -0,0 +1,13 @@
+import { placingMarbles } from "./placingMarbles"
+
+describe.each([
+  ["001", 1],
+  ["101", 2],
+  ["111", 3],
+  ["000", 0]
+])("PlacingMarbles", (a, expected) => {
+    test(`main` ,() => {
+      expect(placingMarbles(a)).toBe(expected)
+    })
+  }
+)
