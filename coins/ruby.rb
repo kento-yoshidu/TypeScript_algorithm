6c7a94a1f675b280b29c85366cbfb7ad20f77b0e
kento <kento@toriwatari.bird>
2022-04-07 19:28:05 +0900

    ✨ Update

--- coins/ruby.rb
+++ coins/ruby.rb
@@ -1,18 +1,13 @@
 def coins(coin1, coin2, coin3, x)
   count = 0
-  roopCount = 0
 
   (0..coin1).each {|a|
     (0..coin2).each {|b|
       (0..coin3).each {|c|
-        roopCount += 1
         (x == (500*a + 100*b + 50*c)) && count += 1
       }
     }
   }
 
-  puts count 
-  puts roopCount
+  return count
 end
-
-coins(1, 5, 10, 500)
