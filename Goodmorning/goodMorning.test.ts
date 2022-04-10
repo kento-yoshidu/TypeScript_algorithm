ec81f1a2b5e26749a2443ecbc5eb17bcba9d1679
kento <kento@toriwatari.bird>
2022-04-10 19:52:27 +0900

    ✨ Update

---
+++ Goodmorning/goodMorning.test.ts
@@ -0,0 +1,11 @@
+import { goodMorning } from "./goodMorning"
+
+describe.each([
+  ["7 0 6 30", "Aoki"],
+  ["7 30 7 30", "Takahashi"],
+  ["0 0 23 59", "Takahashi"]
+])("goodMorning", (a, expected) => {
+  test("main", () => {
+    expect(goodMorning(a)).toBe(expected)
+  }) 
+})
