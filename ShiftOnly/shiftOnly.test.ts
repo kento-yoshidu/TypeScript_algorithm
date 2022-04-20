50ea124eb874867de100e1e4d9ad3579b2c80bde
kento <kento@toriwatari.bird>
2022-04-21 00:19:13 +0900

    ✨ Update

--- ShiftOnly/shiftOnly.test.ts
+++ ShiftOnly/shiftOnly.test.ts
@@ -1,11 +1,17 @@
-import { shiftOnly } from "./shiftOnly"
+import {
+  shiftOnly,
+  shiftOnly2
+} from "./shiftOnly"
 
 describe.each([
   [3, "8 12 40", 2],
   [4, "5 6 8 10", 0],
   [6, "382253568 723152896 37802240 379425024 404894720 471526144", 8]
-])("main", (a, b, expected) => {
-  test("ShiftOnly", () => {
+])("ShiftOnly", (a, b, expected) => {
+  test("first", () => {
     expect(shiftOnly(a, b)).toBe(expected)
+  })
+  test("second", () => {
+    expect(shiftOnly2(a, b)).toBe(expected)
   })
 })
--- ShiftOnly/shiftOnly.ts
+++ ShiftOnly/shiftOnly.ts
@@ -9,3 +9,17 @@ export const shiftOnly = (_: number, nums: string) => {
 
   return count
 }
+
+/* ************************* */
+
+function shift(array: number[], count: number): number {
+  return array.every((num) => num % 2 === 0)
+    ? shift(array.map((n) => n >> 1), count + 1)
+    : count
+}
+
+export const shiftOnly2 = (_: number, nums: string) => {
+  const array = nums.split(' ').map((n) => parseInt(n, 10));
+
+  return shift(array, 0)
+}
