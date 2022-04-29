9538f70f189d1389fb50708e02dd93b616491ba9
kento <kento@toriwatari.bird>
2022-04-29 19:55:29 +0900

    ✨ Update

---
+++ MultiplicationTable/multiplicationTable.ts
@@ -0,0 +1,7 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+function multiplicationTable(array: number[]) {
+  return array.map(a => array.map(b => a * b ))
+}
+
+console.table(multiplicationTable(array))
