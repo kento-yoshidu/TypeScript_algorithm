db5e086db27859a86dbf989191d6be486d3cbabc
kento <kento@toriwatari.bird>
2022-06-04 11:39:17 +0900

    ✨ Update

---
+++ ASCIICode/ASCIICode.ts
@@ -0,0 +1,3 @@
+export const ASCIICode = (input: string) => {
+  return String.fromCharCode(Number(input))
+}
--- aLastCard/aLastCard.ts
+++ aLastCard/aLastCard.ts
@@ -5,11 +5,3 @@ export const aLastCard = (input: string) => {
 
   return result === 0 ? N : result
 }
-
-aLastCard("3 3 2") //=> 1gg
-aLastCard("3 3 3") //=> 2
-aLastCard("3 3 1") //=> 3
-aLastCard("1 100 1") //=> 1
-aLastCard("3 14 2") //=> 3
-
-
