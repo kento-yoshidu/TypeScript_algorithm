7069f7d852f3782f1dff9f77ddda502c53fc002b
kento <kento@toriwatari.bird>
2022-11-24 19:36:26 +0900

    ✨ Update

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -4,9 +4,12 @@ mod euclidean_algorithm;
 mod rightmost;
 mod abc086_a_product;
 mod abc169_a_multiplication;
-*/
 mod abc153_a_serval_vs_monster;
+*/
+mod abc165_a_we_love_golf;
 
 fn main() {
     println!("Hello Rust!");
+
+    abc165_a_we_love_golf::run(1, 11, 11);
 }
