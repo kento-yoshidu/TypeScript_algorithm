e5fee7a15563eb6ae7e34a71e5dd7a9076760220
kento <kento@toriwatari.bird>
2022-11-27 12:52:59 +0900

    ✨ Update

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -9,23 +9,26 @@ mod abc086_a_product;
 mod abc153_a_serval_vs_monster;
 
 mod abc162_b_fizzbuzz_sum;
-*/
 
 mod abc164_b_battle;
-/*
+
 mod abc165_a_we_love_golf;
+
 mod abc165_b_one_percent;
 
 mod abc169_a_multiplication;
 
 mod abc206_b_savings;
+*/
 
+mod abc208_b_factorial_yen_coin;
+
+/*
 mod abc216_a_quiz2;
 
 mod abc200_b_200th;
 */
 
 fn main() {
-    // println!("{}", abc165_b_one_percent::run(103.0));
-    println!("{}", abc164_b_battle::run(10, 9, 10, 10));
+    println!("{}", abc208_b_factorial_yen_coin::run(10000000))
 }
