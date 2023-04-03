c1806d513e6528ca3f3af59667a477a64b25c3a5
kento <kento@toriwatari.bird>
2023-04-04 08:26:50 +0900

    ✨ Update

--- Rust/src/ABC137/a_plus_minus_x.rs
+++ Rust/src/ABC137/a_plus_minus_x.rs
@@ -1,9 +1,19 @@
-/*
-pub fn run<'a>(a: &'a i32, b: &i32) -> &'a i32 {
+pub fn run(a: i32, b: i32) -> i32 {
     let vec = vec![a + b, a - b, a * b];
 
-    let max = vec.iter().max().unwrap();
+    let max = vec.iter().copied().max().unwrap();
 
     max
 }
-*/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(-10, run(-13, 3));
+        assert_eq!(34, run(1, -33));
+        assert_eq!(39, run(13, 3));
+    }
+}
