3186fb30fbb658b747b6d630df5d8fdff65ae0ca
kento <kento@toriwatari.bird>
2023-04-08 07:39:18 +0900

    ✨ Update

---
+++ CommonLisp/Atcoder/abc086/a_product.lisp
@@ -0,0 +1,9 @@
+; aとbの積を2で割り、余りが0ならEven、0でないなｋらOddを返す
+(defun product (a b)
+  (if (eq (mod (* a b) 2) 0)
+    'Even
+    'Odd))
+
+(print (eq 'Even (product 3 4)))
+(print (eq 'Odd (product 1 21)))
+(print (eq 'Odd (product 3 21)))
