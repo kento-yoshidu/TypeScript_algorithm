57aee18ddb0dddf65c7b9631e1a8736178abde5b
kento <kento@toriwatari.bird>
2023-04-15 12:57:52 +0900

    🌚 Update

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -3,8 +3,24 @@
     abc248_a
 */
 
+/* AtCoder Template
+// use proconio::input;
+
+fn main() {
+    input! {
+        a: i32,
+        p: i32
+    }
+    let all_p = a * 3 +p;
+    println!("{}", all_p / 2);
+}
+*/
+
 mod atcoder;
 
 fn main() {
     println!("Hello World");
+    atcoder::abc128::a_apple_pie::run(1, 3);
+    atcoder::abc128::a_apple_pie::run(0, 1);
+    atcoder::abc128::a_apple_pie::run(32, 21);
 }
