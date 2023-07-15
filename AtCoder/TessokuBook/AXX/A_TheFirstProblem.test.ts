5727f52c342abc03617b3be084a3dd05ef957600
kento <kento@toriwatari.bird>
2023-07-15 20:20:40 +0900

    🤑 Update

---
+++ AtCoder/TessokuBook/AXX/A_TheFirstProblem.test.ts
@@ -0,0 +1,11 @@
+import { theFirstBook } from "./A_TheFirstProblem"
+
+describe.each([
+  [2, 4],
+  [8, 64],
+  [100, 10000]
+])("TheFirstProblem", (a, expected) => {
+  test("main", () => {
+    expect(theFirstBook(a)).toBe(expected)
+  })
+})
