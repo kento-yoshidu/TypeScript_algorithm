e21d7068b3246a14e8d842696610c1e634aa1bf6
kento <kento@toriwatari.bird>
2023-09-10 12:05:24 +0900

    ⚾ Update

---
+++ AtCoder/ABC0xx/ABC022/A_BestBody/bestBody.ts
@@ -0,0 +1,18 @@
+// https://atcoder.jp/contests/abc022/tasks/abc022_a
+
+export const bestBody = (n: number, s: number, t: number, w: number[]) => {
+  const arr: number[] = []
+
+  w.map((w, index) => {
+    if (index === 0) {
+      arr.push(w)
+    } else {
+      arr.push(arr[index-1] + w)
+    }
+  })
+
+  return arr.filter((weight) => {
+    return s <= weight && weight <= t
+  }).length
+}
+
